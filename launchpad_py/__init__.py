267be831057d5370af8199ef254facd9d0d7b7d8
Malik Enes Safak <e.maliksafak@gmail.com>
2020-08-28 13:51:15 +0300

    Launchpad Pro MK3 support

--- launchpad_py/__init__.py
+++ launchpad_py/__init__.py
@@ -9,4 +9,5 @@ from launchpad_py.launchpad import Dicer
 from launchpad_py.launchpad import LaunchpadMiniMk3
 from launchpad_py.launchpad import LaunchpadLPX
 from launchpad_py.launchpad import MidiFighter64
+from launchpad_py.launchpad import LaunchpadProMk3
 from launchpad_py import charset
